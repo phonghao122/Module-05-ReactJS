86ff94032f3b4ef8e22f863319d9347f2b365f36
phonghao122 <phonghao122@gmail.com>
2026-05-18 21:05:07 +0700

    ss3

--- ss3/list_students/src/App.jsx
+++ ss3/list_students/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap.js"
-import HeaderComponent from "./component/HeaderComponent.jsx";
 import ListComponent from "./component/ListComponent.jsx";
 
 function App() {
